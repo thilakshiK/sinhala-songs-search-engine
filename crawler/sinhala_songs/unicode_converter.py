1f4145afa9935554011024b3b1bef93ff545d101
Thilakshi Fonseka <2014com2211@gmail.com>
2020-07-03 14:34:28 +0530

    delete crawler

--- crawler/sinhala_songs/unicode_converter.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('./output.json') as fp:
-    data = json.load(fp)
-
-with open('./sinhala_lyrics.json', 'w', encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent =2)
